b87e4124322fd277e2fbaeb2c5dd185e4a0434a3
Vasylyna Sidei <vasilisasidey13@gmail.com>
2026-01-27 18:08:51 +0100

    add styling background for button connect

--- src/components/MobileContactMenu.jsx
+++ src/components/MobileContactMenu.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FiPhone, FiMail, FiMoreHorizontal } from "react-icons/fi";
 import { FaWhatsapp } from "react-icons/fa";
-import "./mobileContactMenu.scss";
+import './mobileContactMenu.scss'
 
 const MobileContactMenu = () => {
   const [open, setOpen] = useState(false);
@@ -19,31 +19,33 @@ const MobileContactMenu = () => {
   if (!visible) return null;
 
   return (
-    <div className={`mobile-menu ${open ? "open" : ""}`}>
-      <a
-        href="https://wa.me/+49039916734060"
-        className="menu-item whatsapp"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <FaWhatsapp />
-      </a>
+    <>
+      {/* Затемнение фона */}
+      {open && <div className="mobile-menu-overlay" onClick={() => setOpen(false)} />}
 
-      <a href="tel:+49039916734060" className="menu-item phone">
-        <FiPhone />
-      </a>
+      <div className={`mobile-menu ${open ? "open" : ""}`}>
+        <a
+          href="https://wa.me/+49039916734060"
+          className="menu-item whatsapp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaWhatsapp />
+        </a>
 
-      <a
-        href="mailto:gundpmeisterwerkstatt@t-online.de"
-        className="menu-item mail"
-      >
-        <FiMail />
-      </a>
+        <a href="tel:+49039916734060" className="menu-item phone">
+          <FiPhone />
+        </a>
 
-      <button className="menu-toggle" onClick={() => setOpen(!open)}>
-        <FiMoreHorizontal />
-      </button>
-    </div>
+        <a href="mailto:info@example.com" className="menu-item mail">
+          <FiMail />
+        </a>
+
+        <button className="menu-toggle" onClick={() => setOpen(!open)}>
+          <FiMoreHorizontal />
+        </button>
+      </div>
+    </>
   );
 };
 
